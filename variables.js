488c1215abdd4218ebb4dfb51d764077be9267d4
Aditya <adijondhale13@gmail.com>
2025-07-04 12:54:57 +0530

    final code

--- variables.js
+++ variables.js
@@ -3,7 +3,20 @@ let accountEmail="a@gmail.com"
 var accountPass="12345"
 
 accountCity="New York"
+let accountState;
 
-accoutnID=2
-
+// accoutnID=2
+ 
+accountEmail="s@gmail.com"
+accountPass="12345678"
+accountCity="Los Angeles"
 console.log(accoutnID)
+
+console.table([accoutnID,accountEmail,accountPass,accountCity,accountState])
+
+/*
+prefer not to use var, use let or const instead
+const is used for constant values, which cannot be reassigned
+let is used for variables that can be reassigned
+var is used for variables that can be reassigned, but it has function scope, not block
+*/
